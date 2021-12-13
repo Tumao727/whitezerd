0ae4c22ec08b7aa3f8de8de6e656049448f4f42a
tumao <yan.wang@zilliz.com>
2021-12-13 16:27:47 +0800

    Add towhee quick start to test katacoda

---
+++ src/components/katacoda/button.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react"
+import { init } from "./katacoda-embed"
+
+const KatacodaBtn = () => {
+  const handleBtnClick = () => {
+    console.log("window katacoda", window.katacoda)
+    window.katacoda && window.katacoda.init()
+  }
+
+  useEffect(() => {
+    // init katacoda
+    init()
+  }, [])
+
+  return (
+    <>
+      {/* <script src="./katacoda-embed.js"></script> */}
+
+      <div
+        id="katacoda-terminal"
+        data-katacoda-ondemand="true"
+        // data-katacoda-port="30000"
+        data-katacoda-env="bash"
+        // data-katacoda-command="start.sh"
+        data-katacoda-ui="panel"
+      ></div>
+      <button onClick={handleBtnClick}>Launch Terminal</button>
+    </>
+  )
+}
+
+export default KatacodaBtn
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -25,6 +25,7 @@ const BlogPostTemplate = ({ data, location }) => {
           <h1 itemProp="headline">{post.frontmatter.title}</h1>
           <p>{post.frontmatter.date}</p>
         </header>
+
         <section
           dangerouslySetInnerHTML={{ __html: post.html }}
           itemProp="articleBody"
